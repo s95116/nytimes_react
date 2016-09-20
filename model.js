5cbca2d8d90307c661d92e1e51ee1ad3780f8dab
Seth Keel <seth.keel@gmail.com>
2016-09-20 15:13:42 -0400

    adding mongoose schema

---
+++ model.js
@@ -0,0 +1,19 @@
+//NYTimes MongoDB Schema
+
+//global dependencies
+var mongoose = require("mongoose");
+
+//connection to local mongo database
+mongoose.connect("mongodb://localhost/nytreact");
+
+//save the mongoose connection to the variable db
+//test to see if the connection was successful
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function(){
+  //if you get the "open" message, the connection was successful
+})
+
+var Schema = mongoose.Schema
+
+var newArticle = new Article({name:"", title:"", url:""})
